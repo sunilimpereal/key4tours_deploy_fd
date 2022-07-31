a24168c0c514f227f6df096ffdee2601dbd04fcd
sunilimpereal <sunilhari496@gmail.com>
2022-07-31 09:07:37 +0530

    new rel

--- key4tours_backend/settings.py
+++ key4tours_backend/settings.py
@@ -200,7 +200,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    # "https://example.com",
+    "https://key4tours.com",
     # "https://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
